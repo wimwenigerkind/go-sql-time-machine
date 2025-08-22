0e9d0dfe532d5ffbf98aeebb8fe2ba57a85f3eb7
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-08-22 13:19:46 +0200

    Implement storage backend initialization in NewBinlogListener function

--- internal/database/mysql/listener.go
+++ internal/database/mysql/listener.go
@@ -4,9 +4,12 @@ import (
 	"fmt"
 	"github.com/go-mysql-org/go-mysql/replication"
 	"github.com/wimwenigerkind/go-sql-time-machine/internal/config"
+	"github.com/wimwenigerkind/go-sql-time-machine/internal/storage"
 )
 
-func NewBinlogListener(cfg *config.Config) {
+// TODO: implement BinlogListener struct
+
+func NewBinlogListener(cfg *config.Config) error {
 	syncerConfig := replication.BinlogSyncerConfig{
 		ServerID: cfg.MySQL.ServerId,
 		Flavor:   "mysql",
@@ -20,6 +23,20 @@ func NewBinlogListener(cfg *config.Config) {
 	fmt.Println(syncer)
 
 	// TODO: initialize storage backends
+	var backends []storage.Backend
+	if len(backends) == 0 {
+		return fmt.Errorf("no storage backends configured")
+	}
+	for _, storageConfig := range cfg.Storage {
+		backend, err := storage.NewBackend(nil, &storageConfig)
+		if err != nil {
+			fmt.Printf("Error initializing storage backend: %v\n", err)
+			continue
+		}
+		backends = append(backends, backend)
+	}
 
 	// TODO: implement WAL
+
+	return nil
 }
