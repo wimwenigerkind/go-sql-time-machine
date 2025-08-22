c5fc9fadcd2c67bc9a671f1748cf1fc1179fc0a1
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-08-22 12:02:47 +0200

    Add Write method to LocalBackend for file storage

--- internal/storage/local.go
+++ internal/storage/local.go
@@ -3,6 +3,13 @@ Copyright © 2025 Wim Wenigerkind <wenigerkind@heptacom.de>
 */
 package storage
 
+import (
+	"context"
+	"io"
+	"os"
+	"path/filepath"
+)
+
 type LocalBackend struct {
 	basePath string
 }
@@ -11,4 +18,21 @@ func NewLocalBackend(basePath string) *LocalBackend {
 	return &LocalBackend{
 		basePath: basePath,
 	}
+}
+
+func (l *LocalBackend) Write(ctx context.Context, key string, reader io.Reader) error {
+	fullPath := filepath.Join(l.basePath, key)
+
+	if err := os.MkdirAll(filepath.Dir(fullPath), 0755); err != nil {
+		return err
+	}
+
+	file, err := os.Create(fullPath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = io.Copy(file, reader)
+	return err
 }
