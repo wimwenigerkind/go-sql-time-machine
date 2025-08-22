86c2e8b7700a7c28dbe991a8aa8298b1938e7c49
Wim Wenigerkind <wim.wenigerkind@heptacom.de>
2025-08-22 11:30:49 +0200

    Add basic MySQL config

---
+++ internal/config/config.go
@@ -0,0 +1,36 @@
+/*
+Copyright © 2025 Wim Wenigerkind <wenigerkind@heptacom.de>
+*/
+package config
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	MySQL MySQLConfig `yaml:"mysql"`
+}
+type MySQLConfig struct {
+	Host     string `yaml:"host"`
+	Port     int    `yaml:"port"`
+	Username string `yaml:"username"`
+	Password string `yaml:"password"`
+	ServerId string `yaml:"server_id"`
+}
+
+func Load(path string) (*Config, error) {
+	v := viper.New()
+	v.SetConfigFile(path)
+	v.SetConfigType("yaml")
+
+	if err := v.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	config := &Config{}
+	if err := v.Unmarshal(config); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
